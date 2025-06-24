c28608c58a5f09e9914e33394860be7d655f9b07
ajubhatti <90272832+ajubhatti@users.noreply.github.com>
2025-06-24 13:30:28 +0530

    Add anula_nexus_mlnm/tailwind.config.ts 1750752028110

---
+++ anula_nexus_mlnm/tailwind.config.ts
@@ -0,0 +1,28 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      spacing: {
+        '128': '32rem',
+      },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: '1rem',
+          sm: '2rem',
+          lg: '4rem',
+          xl: '5rem',
+          '2xl': '6rem',
+        },
+      },
+    },
+  },
+  plugins: [],
+}
+export default config
